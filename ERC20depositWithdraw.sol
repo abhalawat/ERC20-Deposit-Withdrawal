d249401e7b22c38ecab527d37af3cdf7971d7228
abhalawat <97433422+abhalawat@users.noreply.github.com>
2022-01-10 13:32:21 +0530

    Create ERC20depositWithdraw.sol

---
+++ ERC20depositWithdraw.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.4.25;
+
+contract DW {
+    int bal;
+    uint deadline;
+     uint today;
+
+
+    constructor() public{
+        bal =1;
+        today = block.timestamp;
+    }
+
+    function getBalance() view public returns(int){
+        return bal;
+    }
+
+    function deposit(int amt) public {
+        bal = bal +amt;
+        }
+
+
+    function withdraw(int amt) public {
+        require(block.timestamp >= today + 3600 seconds);
+        bal = bal - amt;
+    }
+}
