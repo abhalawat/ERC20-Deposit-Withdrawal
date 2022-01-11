b5285967cc59731b2835cbb0c008bb4ee1e73199
abhalawat <97433422+abhalawat@users.noreply.github.com>
2022-01-11 13:19:39 +0530

    Update ERC20depositWithdraw.sol

--- ERC20depositWithdraw.sol
+++ ERC20depositWithdraw.sol
@@ -1,30 +1,36 @@
 pragma solidity 0.8.11;
 
 contract DW {
+    
     int bal;
     uint deadline;
     uint today;
     address owner;
 
-
-    constructor() public{
+    constructor() public
+    {
         bal = 1;
         owner = msg.sender;
         today = block.timestamp;
     }
-    function getAddress() view public returns(address){
+    
+    function getAddress() view public returns(address)
+    {
         return msg.sender;
     }
-    function getBalance() view public returns(int){
+    
+    function getBalance() view public returns(int)
+    {
         return bal;
     }
 
-    function deposit(int amt) public {
+    function deposit(int amt) public 
+    {
         bal = bal +amt;
     }
 
-
-    function withdraw(int amt) public {
+    function withdraw(int amt) public 
+    {
         require(block.timestamp >= today + 3600 seconds);
         require(msg.sender==owner);
         bal = bal - amt;
